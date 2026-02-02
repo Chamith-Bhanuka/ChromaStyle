844e1c6b8cfb748ac7f406b91b641658a4e39c90
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2026-02-02 23:30:06 +0530

    chore(firebase): update firebase.ts service configuration

--- services/firebase.ts
+++ services/firebase.ts
@@ -11,6 +11,7 @@ const firebaseConfig = {
     storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
     messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
     appId: process.env.EXPO_PUBLIC_APP_ID,
+    measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
