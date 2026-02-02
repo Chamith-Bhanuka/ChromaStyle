79656e7f97645411003250adcfbf2ec236d4b7ee
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2026-02-02 23:39:20 +0530

    feat(auth): update Index page to use useAuth for user state and redirects

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,22 @@
 import '../global.css';
 import { Redirect } from "expo-router";
+import { useAuth } from '@/hooks/useAuth';
+import { ActivityIndicator, View } from 'react-native';
 
 export default function Index() {
-    return <Redirect href="/register" />;
+    const { user, loading } = useAuth();
+    
+    if (loading) {
+      return (
+        <View className="flex-1 justify-center items-center bg-gray-50">
+          <ActivityIndicator size="large" color="blue" />
+        </View>
+      );
+    }
+
+    if (user) {
+      return <Redirect href="/home" />;
+    } else {
+      return <Redirect href="/login" />;
+    }
 }
