dd7b664960de5fc1871167c632d6234c92ac4aeb
Chamith-Bhanuka <c.bhanukawp2002@gmail.com>
2026-02-02 23:32:03 +0530

    feat(auth): create authService with registerUser and login functions

---
+++ services/authService.ts
@@ -0,0 +1,31 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from '@firebase/auth';
+import { auth, db } from '@/services/firebase';
+import { doc, setDoc } from '@firebase/firestore';
+
+export const registerUser = async (
+  fullName: string,
+  email: string,
+  password: string
+) => {
+  const userCredential = await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+  await updateProfile(userCredential.user, { displayName: fullName });
+  await setDoc(doc(db, 'users', userCredential.user.uid), {
+    name: fullName,
+    role: '',
+    email,
+    createAt: new Date(),
+  });
+  return userCredential.user;
+};
+
+export const login = async (email: string, password: string) => {
+  return await signInWithEmailAndPassword(auth, email, password);
+};
